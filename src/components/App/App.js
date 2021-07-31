658d969f149a7bdf82bed849ccc4e53670f265b7
Sitharthan <mailtositharthan@gmail.com>
2021-07-31 19:42:43 +0530

    Building Login page component with route concpet as done

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,15 @@
-import { React } from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
+import Login from "../Login/Login";
 import Dashboard from '../Dashboard/Dashboard';
 import Preferences from '../Preferences/Preferences';
 
 function App() {
+    const [token, setToken] = useState();
+    if(!token) {
+        return <Login setToken={setToken} />
+    }
     return (
         <div className="wrapper">
             <h1>Application</h1>
