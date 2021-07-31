9a2606ba291c0c2a6fa37c921062865b2e2eae50
Sitharthan <mailtositharthan@gmail.com>
2021-07-31 19:23:10 +0530

    The browser will reload and you’ll find your basic components and able to check with routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,24 @@
 import { React } from "react";
-import { BrowserRouter, Router, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
-import { Dashboard } from "../Dashboard/Dashboard";
-import { Preferences } from "../Preferences/Preferences";
+import Dashboard from '../Dashboard/Dashboard';
+import Preferences from '../Preferences/Preferences';
 
 function App() {
     return (
-        <></>
+        <div className="wrapper">
+            <h1>Application</h1>
+            <BrowserRouter>
+            <Switch>
+                <Route path="/dashboard">
+                    <Dashboard/>
+                </Route>
+                <Route path="/preferences">
+                    <Preferences/>
+                </Route>
+            </Switch>
+            </BrowserRouter>
+        </div>
     );
 }
 
