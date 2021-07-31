a8f845b6cb599442c6232dc214d04af31558cc12
Sitharthan <mailtositharthan@gmail.com>
2021-07-31 18:57:24 +0530

    Building components for Dashboard, Preferences, App

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import { React } from "react";
+import { BrowserRouter, Router, Switch } from "react-router-dom";
+import "./App.css";
+import { Dashboard } from "../Dashboard/Dashboard";
+import { Preferences } from "../Preferences/Preferences";
+
+function App() {
+    return (
+        <></>
+    );
+}
+
+export default App;
